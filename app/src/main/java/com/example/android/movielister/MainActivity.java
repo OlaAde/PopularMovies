83c3ebf0584aa4d815c979984dc22863488558a3
Adeogo <adeogo.oladipo@gmail.com>
2017-04-10 07:48:49 +0300

    Making UX changes

--- app/src/main/java/com/example/android/movielister/MainActivity.java
+++ app/src/main/java/com/example/android/movielister/MainActivity.java
@@ -9,7 +9,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,16 +34,25 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
     private String sort_by_popularity = "popularity.desc";
     private String sort_by_highest_rated = "vote_average.desc";
-    private String mSortPref = sort_by_popularity;
+    private String mSortPref = null;
 
+    private TextView mErrorMessageDisplay;
+
+    private ProgressBar mLoadingIndicator;
     private String keyPref = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSortPref = sort_by_popularity;
        mRecyclerView = (RecyclerView) findViewById(R.id.rv_main);
         mTextViewTitle = (TextView) findViewById(R.id.tv_item);
         mImageViewitem = (ImageView) findViewById(R.id.iv_item);
+
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+/* This TextView is used to display errors and will be hidden if there are no errors */
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+
 
         GridLayoutManager layoutManager = new GridLayoutManager(this, 3 ,GridLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(layoutManager);
@@ -53,8 +64,37 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
     }
 
     public void updateLayout(){
+        showWeatherDataView();
         mMovieAdapter.setMovieData(null);
         new LoadMoviesTask().execute(mSortPref);
+    }
+
+    /**
+     * This method will make the View for the weather data visible and
+     * hide the error message.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showWeatherDataView() {
+        /* First, make sure the error is invisible */
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        /* Then, make sure the weather data is visible */
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * This method will make the error message visible and hide the weather
+     * View.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showErrorMessage() {
+        /* First, hide the currently visible data */
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        /* Then, show the error */
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -85,6 +125,11 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
 
     public class LoadMoviesTask extends AsyncTask<String,Void , JSONArray>{
+        @Override
+        protected void onPreExecute() {
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+            super.onPreExecute();
+        }
 
         @Override
         protected JSONArray doInBackground(String... strings) {
@@ -109,9 +154,13 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
         @Override
         protected void onPostExecute(JSONArray jArray) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
             if (jArray != null) {
+                showWeatherDataView();
                 mMovieAdapter.setMovieData(jArray);
             }
+            else
+                showErrorMessage();
         }
     }
 
